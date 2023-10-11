6ae88752750de34e63d6e8829c95a1ab0644df7b
Valenciasebas4 <valenciasebastian80@gmail.com>
2023-10-11 08:59:26 -0500

    Create new project

---
+++ Cine/Cine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
