7572f8d93b1d2d919d4508247e738f16b2ef3020
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-10-11 21:39:32 -0500

    Create Controller movies and model AddmovieView

---
+++ Cine/Cine/Controllers/MoviesController.cs
@@ -0,0 +1,40 @@
+﻿using Cine.DAL;
+using Cine.Helpers;
+using Cine.Models;
+using Cine.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cine.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        private readonly DataBaseContext _context;
+        private readonly IDropDownListHelper _dropDownListHelper;
+
+        public MoviesController(DataBaseContext context, IDropDownListHelper dropDownListHelper)
+        {
+            _context = context;
+            _dropDownListHelper = dropDownListHelper;
+        }
+
+        
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Movies.ToListAsync());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            
+            AddMovieViewModel addMovieViewModel = new()
+            {
+                Genders = await _dropDownListHelper.GetDDLGendersAsync(),
+                Classifications = await _dropDownListHelper.GetDDLClassificationsAsync(),   
+            };
+
+            return View(addMovieViewModel);
+        }
+    }
+}
---
+++ Cine/Cine/Models/AddMovieViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine.Models
+{
+    public class AddMovieViewModel
+    {
+        [Key]
+        public virtual int Id { get; set; }
+
+        [Display(Name = "Titulo")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Title { get; set; }
+
+        [Display(Name = "Descripción")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Description { get; set; }
+
+        [Display(Name = "Director")]
+        [MaxLength(50)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string Director { get; set; }
+
+        [Display(Name = "Duracion")]
+        [MaxLength(250)]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int Duration { get; set; }
+
+
+        [Display(Name = "Genero")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int GenderId { get; set; }
+        public IEnumerable<SelectListItem> Genders { get; set; }
+
+        [Display(Name = "Clasificacion")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int ClassificationId { get; set; }
+        public IEnumerable<SelectListItem> Classifications { get; set; }
+    }
+}
