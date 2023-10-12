91178b076068f5c7e42433df31dafbbc90d41711
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-10-11 20:19:23 -0500

    Create folder Helpers and Services with your class DropDownListHelper

---
+++ Cine/Cine/Helpers/IDropDownListHelper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Cine.Helpers
+{
+    public interface IDropDownListHelper
+    {
+        Task<IEnumerable<SelectListItem>> GetDDLRoomsAsync(); //Lista de salas
+        Task<IEnumerable<SelectListItem>> GetDDLSeatsAsync(); //Lista de salas
+        Task<IEnumerable<SelectListItem>> GetDDLClassificationsAsync(); //Lista de salas
+        Task<IEnumerable<SelectListItem>> GetDDLGendersAsync(); //Lista de salas
+
+    }
+}
---
+++ Cine/Cine/Services/DropDownListHelper.cs
@@ -0,0 +1,104 @@
+﻿using Cine.DAL;
+using Cine.Helpers;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cine.Services
+{
+    public class DropDownListHelper : IDropDownListHelper
+    {
+
+        public readonly DataBaseContext _context;
+
+        public DropDownListHelper(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        
+
+        public async Task<IEnumerable<SelectListItem>> GetDDLRoomsAsync()
+        {
+            List<SelectListItem> listRooms = await _context.Rooms
+                .Select(c => new SelectListItem
+                {
+                    Text = c.NumberRoom,
+                    Value = c.Id.ToString(),
+                })
+                .OrderBy(c => c.Text)
+                .ToListAsync();
+
+            listRooms.Insert(0, new SelectListItem
+            {
+                Text = "Seleccione una Sala...",
+                Value = Guid.Empty.ToString(),
+                Selected = true
+            });
+
+            return listRooms;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetDDLSeatsAsync()
+        {
+            List<SelectListItem> listSeats = await _context.Seats
+               .Select(c => new SelectListItem
+               {
+                   Text = c.NumberSeat,
+                   Value = c.Id.ToString(),
+               })
+               .OrderBy(c => c.Text)
+               .ToListAsync();
+
+            listSeats.Insert(0, new SelectListItem
+            {
+                Text = "Seleccione una Silla...",
+                Value = Guid.Empty.ToString(),
+                Selected = true
+            });
+
+            return listSeats;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetDDLClassificationsAsync()
+        {
+            List<SelectListItem> listClassifications = await _context.Classifications
+               .Select(c => new SelectListItem
+               {
+                   Text = c.ClassificationName,
+                   Value = c.Id.ToString(),
+               })
+               .OrderBy(c => c.Text)
+               .ToListAsync();
+
+            listClassifications.Insert(0, new SelectListItem
+            {
+                Text = "Seleccione una Clasificación...",
+                Value = Guid.Empty.ToString(),
+                Selected = true
+            });
+
+            return listClassifications;
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetDDLGendersAsync()
+        {
+            List<SelectListItem> listGenders = await _context.Genders
+              .Select(c => new SelectListItem
+              {
+                  Text = c.GenderName,
+                  Value = c.Id.ToString(),
+              })
+              .OrderBy(c => c.Text)
+              .ToListAsync();
+
+            listGenders.Insert(0, new SelectListItem
+            {
+                Text = "Seleccione un Genero...",
+                Value = Guid.Empty.ToString(),
+                Selected = true
+            });
+
+            return listGenders;
+        }
+    }
+}
