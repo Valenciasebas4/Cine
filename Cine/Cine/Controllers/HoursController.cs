7a2e3f888dae80e38bc4ee2788eaef17b4a3bce7
SEBASTIAN LONDO�O VALENCIA <sebastianlondono264947@correo.itm.edu.co>
2023-10-12 21:11:11 -0500

    Create controller hours and view create and index and model addhourviewmodel

---
+++ Cine/Cine/Controllers/HoursController.cs
@@ -0,0 +1,90 @@
+﻿using Cine.DAL;
+using Cine.DAL.Entities;
+using Cine.Helpers;
+using Cine.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cine.Controllers
+{
+    public class HoursController : Controller
+    {
+        private readonly DataBaseContext _context;
+        private readonly IDropDownListHelper _dropDownListHelper;
+
+        public HoursController(DataBaseContext context, IDropDownListHelper dropDownListHelper)
+        {
+            _context= context;
+            _dropDownListHelper = dropDownListHelper;
+        }
+        // GET: Genders
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Hours.ToListAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+
+            AddHourViewModel addHourViewModel = new()
+            {
+                Movies = await _dropDownListHelper.GetDDLMoviesAsync(),
+                
+            };
+
+            return View(addHourViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AddHourViewModel addHourViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+
+
+                    Hour hour = new()
+                    {
+                        StarTime = addHourViewModel.StarTime,
+                        EndignTime = addHourViewModel.EndignTime,
+                        Date = addHourViewModel.Date,                      
+                        CreatedDate = DateTime.Now,
+                        Movie = await _context.Movies.FindAsync(addHourViewModel.MovieId),
+    
+                    };
+
+
+
+
+
+                    _context.Add(hour);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+
+            addHourViewModel.Movies = await _dropDownListHelper.GetDDLMoviesAsync();
+            
+            return View(addHourViewModel);
+        }
+    }
+}
---
+++ Cine/Cine/Models/AddHourViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Cine.DAL.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Cine.Models
+{
+    public class AddHourViewModel
+    {
+
+        [Display(Name = "Hora de inicio")]
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime StarTime { get; set; }
+
+        [Display(Name = "Hora de Finalización")]
+        [Required]
+        [DataType(DataType.Time)]
+        public DateTime EndignTime { get; set; }
+
+        [Display(Name = "Fecha")]
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "Pelicula")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public int MovieId { get; set; }
+        public IEnumerable<SelectListItem> Movies { get; set; }
+
+       
+    }
+}
